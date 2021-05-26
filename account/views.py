61809178ff3d6de1381864e14b16853bb3087248
min-park-arch <0707369@yonsei.ac.kr>
2021-05-26 19:53:57 +0900

    user확장

--- account/views.py
+++ account/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import authenticate, login, logout
+from .forms import RegisterForm
 
 # Create your views here.
 
@@ -25,11 +26,11 @@ def logout_view(request):
 
 def register_view (request):
   if request.method == "POST":
-    form = UserCreationForm(request.POST)
+    form = RegisterForm(request.POST)
     if form.is_valid():
       user = form.save()
       login(request, user)
     return redirect('home')
   else: 
-    form = UserCreationForm()
+    form = RegisterForm()
     return render(request, 'signup.html', {'form':form})
